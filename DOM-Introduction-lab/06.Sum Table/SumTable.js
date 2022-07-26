5c87e0cfee02dfbfc6e3527aba6580cb5d7fe8a9
c0c3t0 <stoyanka.stoichkova@abv.bg>
2022-07-26 16:37:23 +0300

    DOM Intro

--- DOM-Introduction-lab/06.Sum Table/SumTable.js
+++ DOM-Introduction-lab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    let elements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let elementsArray = Array.from(elements).slice(0, -1);
+    let sumElements = elementsArray.reduce((a, x) => {
+        return a + Number(x.textContent)
+    }, 0);
 
+    let totalElement = document.getElementById('sum');
+    totalElement.textContent = sumElements;
 }
