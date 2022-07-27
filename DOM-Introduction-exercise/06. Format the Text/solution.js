11776021d29305c3d5b1661469b1f366c57ec282
c0c3t0 <stoyanka.stoichkova@abv.bg>
2022-07-27 23:59:33 +0300

    DOM Intro

--- DOM-Introduction-exercise/06. Format the Text/solution.js
+++ DOM-Introduction-exercise/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+    let textElement = document.getElementById('input').value.split('.');
+    let sentances = textElement.filter(sentance => sentance !== '');
+    let output = document.getElementById('output');
+
+    while (sentances.length > 0){
+        let text = sentances.splice(0, 3).join(". ") + ".";
+        let p = document.createElement('p');
+        p.textContent = text;
+        output.appendChild(p);
+    }
 }
--- DOM-Introduction-exercise/07. Hell's Kitchen/app.js
+++ DOM-Introduction-exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,35 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let input = JSON.parse(document.querySelector('#inputs textarea').value);
+      let restaurantInfo = {};
+      let bestRestaurant = '';
+      let averageSalary = 0;
+      let bestSalary = 0;
+
+
+      for (let line of input) {
+         let [restaurantName, restaurantWorkers] = line.split(' - ');
+         let workers = restaurantWorkers.split(", ")
+
+         for (let w of workers) {
+            let [workerName, workerSalary] = w.split(" ");
+            if(!restaurantInfo.hasOwnProperty('restaurantName')){
+               restaurantInfo[restaurantName] = {};
+            } else {
+               restaurantInfo[restaurantName][workerName] = Number(workerSalary);
+            }
+         }
+
+         console.log(restaurantInfo);
+         // console.log(workerName);
+         // console.log(workerSalary);
+
+      }
+
+
+
+
    }
 }
