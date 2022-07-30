3be8218f95aa20133ee998f699924ce93cd2c245
c0c3t0 <stoyanka.stoichkova@abv.bg>
2022-07-31 02:14:20 +0300

    DOM Manipulations and Events

--- 10-DOM-manipulations-and-events-exercise/03. Locked-Profile/app.js
+++ 10-DOM-manipulations-and-events-exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button'))
+        .forEach(btn => btn.addEventListener('click', showMoreHandler));
+
+    function showMoreHandler(e) {
+        let isUnlocked = e.target.parentNode.querySelector('input[value="unlock"]').checked;
+
+        if (isUnlocked) {
+            let hiddenDivElement = Array.from(e.target.parentNode.querySelectorAll('div'))
+                .find(el => el.id.includes('Hidden'));
+
+            if (e.target.textContent === 'Show more') {
+                hiddenDivElement.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                hiddenDivElement.style.display = 'none';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
--- 10-DOM-manipulations-and-events-exercise/04. Fill-Dropdown/dropdown.js
+++ 10-DOM-manipulations-and-events-exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let textInputElement = document.querySelector('#newItemText');
+    let valueInputElement = document.querySelector('#newItemValue');
+    let dropdownElement = document.querySelector('#menu');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = textInputElement.value + " " + valueInputElement.value;
+    dropdownElement.appendChild(optionElement);
+    textInputElement.value = '';
+    valueInputElement.value = '';
+    
 }
