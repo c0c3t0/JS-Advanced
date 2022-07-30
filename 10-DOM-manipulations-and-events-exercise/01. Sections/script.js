c421ae4379b9cbc802224aeeae4aa815f6c16eda
c0c3t0 <stoyanka.stoichkova@abv.bg>
2022-07-30 23:30:12 +0300

    DOM Manipulations and Events

--- 10-DOM-manipulations-and-events-exercise/01. Sections/script.js
+++ 10-DOM-manipulations-and-events-exercise/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentElement = document.querySelector('#content');
+
+   words.forEach(x => {
+      let divCreate = document.createElement('div');
+      let pCreate = document.createElement('p');
+      pCreate.textContent = x;
+      pCreate.style.display = 'none'
+
+      divCreate.addEventListener('click', (e) => {
+         e.target.children[0].style.display = 'block';
+      })
+      
+      contentElement.appendChild(divCreate);
+      divCreate.appendChild(pCreate);
+   });
 }
--- 10-DOM-manipulations-and-events-exercise/02. Time-Converter/solution.js
+++ 10-DOM-manipulations-and-events-exercise/02. Time-Converter/solution.js
@@ -1,4 +1,39 @@
 function attachEventsListeners() {
+    let daysBtnElement = document.querySelector('#daysBtn');
+    let hoursBtnElement = document.querySelector('#hoursBtn');
+    let minutesBtnElement = document.querySelector('#minutesBtn');
+    let secondsBtnElement = document.querySelector('#secondsBtn');
 
-    console.log('TODO:...');
+    let daysInputElement = document.querySelector('#days');
+    let hoursInputElement = document.querySelector('#hours');
+    let minutesInputElement = document.querySelector('#minutes');
+    let secondsInputElement = document.querySelector('#seconds');
+
+    daysBtnElement.addEventListener('click', function () {
+        let days = daysInputElement.value;
+        hoursInputElement.value = days * 24;
+        minutesInputElement.value = days * 1440;
+        secondsInputElement.value = days * 86400;
+    });
+
+    hoursBtnElement.addEventListener('click', function () {
+        let hours = hoursInputElement.value;
+        daysInputElement.value = hours / 24;
+        minutesInputElement.value = hours * 60;
+        secondsInputElement.value = hours * 60 * 60;
+    });
+
+    minutesBtnElement.addEventListener('click', function () {
+        let minutes = minutesInputElement.value;
+        daysInputElement.value = minutes / 60 / 24;
+        hoursInputElement.value = minutes / 60;
+        secondsInputElement.value = minutes * 60;
+    });
+
+    secondsBtnElement.addEventListener('click', function () {
+        let seconds = secondsInputElement.value;
+        daysInputElement.value = seconds / 60 / 60 / 24;
+        hoursInputElement.value = seconds / 60 / 60;
+        minutesInputElement.value = seconds / 60;
+    });
 }
