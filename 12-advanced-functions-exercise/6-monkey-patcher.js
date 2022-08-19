6ad94ab8dd3d6d617911cf2fc5c7e591dd5bd3fd
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-08-19 17:28:20 +0300

    Advanced Functions

---
+++ 12-advanced-functions-exercise/6-monkey-patcher.js
@@ -0,0 +1,59 @@
+function solution(command) {
+
+    if (command === 'upvote') {
+        return this.upvotes++;
+    } else if (command === 'downvote') {
+        return this.downvotes++;
+    }
+
+    let upvotes = this.upvotes;
+    let downvotes = this.downvotes;
+    let totalVotes = upvotes + downvotes;
+    let balance = upvotes - downvotes;
+
+    function ratingState() {
+        if (totalVotes < 10) {
+            return 'new';
+        } else if (upvotes > totalVotes * 0.66) {
+            return 'hot';
+        } else if (balance >= 0 && totalVotes > 100) {
+            return 'controversial'
+        } else if (balance < 0) {
+            return 'unpopular';
+        } else {
+            return 'new';
+        }
+    }
+
+    if (totalVotes > 50) {
+        let inflateVote = Math.ceil(Math.max(upvotes, downvotes) * 0.25);
+        return [upvotes + inflateVote, downvotes + inflateVote, balance, ratingState()];
+    }
+
+    return [upvotes, downvotes, balance, ratingState.call(this)];
+
+}
+
+
+let post = {
+    id: '3',
+    author: 'emil',
+    content: 'wazaaaaa',
+    upvotes: 100,
+    downvotes: 100
+};
+// let post = {
+//     id: '3',
+//     author: 'emil',
+//     content: 'wazaaaaa',
+//     upvotes: 9,
+//     downvotes: 0
+// };
+solution.call(post, 'upvote');
+solution.call(post, 'downvote');
+let score = solution.call(post, 'score');
+console.log(score); // [127, 127, 0, 'controversial']
+solution.call(post, 'downvote');         // (executed 50 times)
+
+score = solution.call(post, 'score');
+console.log(score);    // [139, 189, -50, 'unpopular']
