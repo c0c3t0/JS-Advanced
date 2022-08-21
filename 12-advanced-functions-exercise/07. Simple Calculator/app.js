b34d7014f3df8b49e775254b9f7d033ae8cd9f0b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-08-21 16:41:57 +0300

    DOM-Related Problems

--- 12-advanced-functions-exercise/07. Simple Calculator/app.js
+++ 12-advanced-functions-exercise/07. Simple Calculator/app.js
@@ -1,8 +1,4 @@
 function calculator() {
-    // let firstElement;
-    // let secondElement;
-    // let resultElement;
-
     return {
         init(selector1, selector2, resultSelector) {
             firstElement = document.querySelector(selector1);
@@ -21,6 +17,3 @@ function calculator() {
 
 const calculate = calculator();
 calculate.init('#num1', '#num2', '#result');
-//
-
-
--- 12-advanced-functions-exercise/08. Next Article/script.js
+++ 12-advanced-functions-exercise/08. Next Article/script.js
@@ -1,3 +1,13 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+    let articles = input;
+    let divElement = document.querySelector('#content');
+
+    return () => {
+        if (articles.length) {
+            let createArticle = document.createElement('article');
+            createArticle.textContent = articles.shift();
+            createArticle.style.display = 'block';
+            divElement.appendChild(createArticle);
+        }
+    }
 }
