2ebbbad99854bdca4171f7e37b377852b86b27ee
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-08-29 23:46:08 +0300

    Classes

---
+++ 15-classes-lab/4. pointDistance.js
@@ -0,0 +1,16 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    static distance(point1, point2) {
+        let a = point1.x - point2.x;
+        let b = point1.y - point2.y;
+
+        return Math.sqrt(a ** 2 + b ** 2);
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
