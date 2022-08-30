75f5ab38eb60e340711e30905212eb2b03faca8b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-08-30 17:49:19 +0300

    Classes

---
+++ 16-classes-exercise/04. sortedList.js
@@ -0,0 +1,36 @@
+class List {
+    constructor() {
+        this.list = [];
+        this.size = this.list.lenght;
+    }
+
+    add(element) {
+        this.list.push(Number(element));
+        this.list = this.list.sort((a, b) => a - b);
+        this.size = this.list.length;
+    }
+
+    remove(index) {
+        if (index < 0 || index > this.list.length) {
+            throw new Error('Index out of range');
+        }
+        this.list.splice(index, 1);
+        this.list = this.list.sort((a, b) => a - b);
+        this.size = this.list.length;
+    }
+
+    get(index) {
+        if (index < 0 || index > this.list.length) {
+            throw new Error('Index out of range');
+        }
+        return this.list[index];
+    }
+}
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1));
+list.remove(1);
+console.log(list.get(1));
