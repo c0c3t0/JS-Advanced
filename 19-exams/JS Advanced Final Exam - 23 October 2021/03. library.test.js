6dd5fc17e394c5bf98899087dca5dbc61f38df9b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-07 09:12:01 +0300

    Problem Solution

--- 19-exams/JS Advanced Final Exam - 23 October 2021/03. library.test.js
+++ 19-exams/JS Advanced Final Exam - 23 October 2021/03. library.test.js
@@ -21,15 +21,13 @@ describe('library tests', () => {
         });
 
         it('expect 50% discount of book', () => {
-            expect(library.calcPriceOfBook('book', 1900)).to.equal('Price of book is 10.00')
-            expect(library.calcPriceOfBook('book', 1980)).to.equal('Price of book is 10.00')
-
+            expect(library.calcPriceOfBook('book', 1900)).to.equal('Price of book is 10.00');
+            expect(library.calcPriceOfBook('book', 1980)).to.equal('Price of book is 10.00');
         });
 
         it('expect of book to be 20BGN', () => {
-            expect(library.calcPriceOfBook('book', 2000)).to.equal('Price of book is 20.00')
-            expect(library.calcPriceOfBook('book', 1981)).to.equal('Price of book is 20.00')
-
+            expect(library.calcPriceOfBook('book', 2000)).to.equal('Price of book is 20.00');
+            expect(library.calcPriceOfBook('book', 1981)).to.equal('Price of book is 20.00');
         });
     });
 
@@ -42,20 +40,32 @@ describe('library tests', () => {
         it('should return a proper message if book is not in the library', () => {
             expect(library.findBook(["Troy", "Life Style", "Torronto"], 'book')).to.equal('The book you are looking for is not here!');
             expect(library.findBook(["Troy", "Life Style", "Torronto"], 'torronto')).to.equal('The book you are looking for is not here!');
-
         });
 
         it('should return a proper message if book is in the library', () => {
             expect(library.findBook(["Troy", "Life Style", "Torronto"], 'Troy')).to.equal('We found the book you want.');
             expect(library.findBook(["Troy", "Life Style", "Torronto"], 'Torronto')).to.equal('We found the book you want.');
-
         });
     });
 
     describe('arrangeTheBooks(countBooks)', () => {
-        it('', () => {
+        it('should throw an error if input is invalid', () => {
+            expect(() => { library.arrangeTheBooks('') }).to.throw('Invalid input');
+            expect(() => { library.arrangeTheBooks({}) }).to.throw('Invalid input');
+            expect(() => { library.arrangeTheBooks(undefined) }).to.throw('Invalid input');
+            expect(() => { library.arrangeTheBooks(true) }).to.throw('Invalid input');
+            expect(() => { library.arrangeTheBooks(-3) }).to.throw('Invalid input');
+            expect(() => { library.arrangeTheBooks(-3.3) }).to.throw('Invalid input');
+            expect(() => { library.arrangeTheBooks(3.3) }).to.throw('Invalid input');
+        });
 
+        it('should return proper message when book shelves is enough', () => {
+            expect(library.arrangeTheBooks(40)).to.equal('Great job, the books are arranged.');
+            expect(library.arrangeTheBooks(0)).to.equal('Great job, the books are arranged.');
+        });
+
+        it('should return proper message when book shelves is not enough', () => {
+            expect(library.arrangeTheBooks(41)).to.equal('Insufficient space, more shelves need to be purchased.');
         });
     });
-
 })
