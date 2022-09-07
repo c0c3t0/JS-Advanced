726250fbc3e96780e52fdcab2938c929f08d0de0
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-07 10:04:50 +0300

    Problem Solution

--- 19-exams/JS Advanced Final Exam - 23 October 2021/02. summerCamp.js
+++ 19-exams/JS Advanced Final Exam - 23 October 2021/02. summerCamp.js
@@ -11,25 +11,33 @@ class SummerCamp {
     }
 
     registerParticipant(name, condition, money) {
-        let foundCondition = Object.keys(this.priceForTheCamp).find(el => el === condition);
+        let foundCondition = Object.keys(this.priceForTheCamp).find(key => key === condition);
         if (!foundCondition) {
             throw new Error('Unsuccessful registration at the camp.');
         }
 
         let foundInListOfParticipants = this.listOfParticipants.find(el => el.name === name);
-        if(foundInListOfParticipants) {
+        if (foundInListOfParticipants) {
             return `The ${name} is already registered at the camp.`
         }
 
         if (money < this.priceForTheCamp[foundCondition]) {
             return 'The money is not enough to pay the stay at the camp.';
         }
-        this.listOfParticipants.push({name, condition, power: 100, wins: 0});
+        this.listOfParticipants.push({ name, condition, power: 100, wins: 0 });
         return `The ${name} was successfully registered.`;
     }
 
     unregisterParticipant(name) {
+        let foundInListOfParticipants = this.listOfParticipants.find(el => el.name === name);
+        if (!foundInListOfParticipants) {
+            throw new Error(`The ${name} is not registered in the camp.`);
+        }
+        console.log(this.listOfParticipants);
+        this.listOfParticipants = this.listOfParticipants.filter(participant => participant.name !== name);
+        console.log(this.listOfParticipants);
 
+        return `The ${name} removed successfully.`
     }
 
     timeToPlay(typeOfGame, participant1, participant2) {
@@ -41,8 +49,14 @@ class SummerCamp {
     }
 }
 
+// const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
+
 const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
-console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
 console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
-console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
-console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
+console.log(summerCamp.unregisterParticipant("Petar Petarson"));
+console.log(summerCamp.unregisterParticipant("Petar"));
+
