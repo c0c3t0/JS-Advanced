23c5a10154ae86e3edc70dce2215b2e2a17d418d
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-09 00:32:06 +0300

    Problem Solution

--- 19-exams/JS Advanced Final Retake Exam - 12 August 2021/01. Furniture Store/app.js
+++ 19-exams/JS Advanced Final Retake Exam - 12 August 2021/01. Furniture Store/app.js
@@ -1,5 +1,3 @@
-window.addEventListener('load', solve);
-
 function solve() {
     let model = document.querySelector('#model');
     let year = document.querySelector('#year');
@@ -8,34 +6,53 @@ function solve() {
     let addBtn = document.querySelector('#add');
 
     let furnitureList = document.querySelector('#furniture-list');
+    let totalPrice = document.querySelector('.total-price');
 
     addBtn.addEventListener('click', validateAndAdd);
 
     function validateAndAdd(e) {
-        if (!model.value || !year.value || ! description.value || !price.value) {
-            return;
+        if (model.value !== '' && year.value !== '' && description.value !== '' && price.value >= 0 
+        && Number(price.value) > 0 && Number(year.value) > 0) {
+            add(e, model.value, year.value, description.value, price.value);
+            clearInputs();
         }
-        add(e, model.value, year.value, description.value, price.value);
-        clearInputs();
-
-    
     }
 
     function add(e, model, year, description, price) {
         e.preventDefault();
 
-        let tr = htmlGenerator('tr', '', furnitureList);
-        tr.setAttribute('class', 'info');
-        htmlGenerator('td', `${model}`, tr);
-        htmlGenerator('td', `${Number(price).toFixed(2)}`, tr);
-        let td = htmlGenerator('td', '', tr);
+        let firstTr = htmlGenerator('tr', '', furnitureList);
+        firstTr.setAttribute('class', 'info');
+        htmlGenerator('td', `${model}`, firstTr);
+        htmlGenerator('td', `${Number(price).toFixed(2)}`, firstTr);
+        let td = htmlGenerator('td', '', firstTr);
 
         let moreBtn = htmlGenerator('button', 'More Info', td);
         moreBtn.setAttribute('class', 'moreBtn');
 
-        let buyBtn = htmlGenerator('button', 'Buy It', td);
+        let buyBtn = htmlGenerator('button', 'Buy it', td);
         buyBtn.setAttribute('class', 'buyBtn');
 
+        let secondTr = htmlGenerator('tr', '', furnitureList);
+        secondTr.setAttribute('class', 'hide');
+        htmlGenerator('td', `Year: ${year}`, secondTr);
+        let descriptionTd = htmlGenerator('td', `Description: ${description}`, secondTr);
+        descriptionTd.setAttribute('colspan', 3);
+
+        moreBtn.addEventListener('click', (e) => {
+            e.target.textContent === 'More Info'
+                ? e.target.textContent = 'Less Info'
+                : e.target.textContent = 'More Info';
+            secondTr.style.display = 'none'
+                ? secondTr.style.display = 'contents'
+                : secondTr.style.display = 'none';
+        })
+
+        buyBtn.addEventListener('click', (e) => {
+            firstTr.remove();
+            secondTr.remove();
+            totalPrice.textContent = (Number(totalPrice.textContent) + Number(price)).toFixed(2);
+        })
     }
 
     function htmlGenerator(tagName, content, parent) {
