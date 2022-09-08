3a4274dd0a8d6781a7c81ece6a9885761e16e8da
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-08 15:51:58 +0300

    Problem Solution

---
+++ 19-exams/JS Advanced Final Retake Exam - 12 August 2021/01. Furniture Store/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', solve);
+
+function solve() {
+    let model = document.querySelector('#model');
+    let year = document.querySelector('#year');
+    let description = document.querySelector('#description');
+    let price = document.querySelector('#price');
+    let addBtn = document.querySelector('#add');
+}
