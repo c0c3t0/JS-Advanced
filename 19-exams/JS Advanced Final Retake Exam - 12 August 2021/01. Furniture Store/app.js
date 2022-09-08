057bda37235dd61c744684a961cfe824d1c70bc8
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-08 23:06:16 +0300

    Problem Solution

--- 19-exams/JS Advanced Final Retake Exam - 12 August 2021/01. Furniture Store/app.js
+++ 19-exams/JS Advanced Final Retake Exam - 12 August 2021/01. Furniture Store/app.js
@@ -6,4 +6,51 @@ function solve() {
     let description = document.querySelector('#description');
     let price = document.querySelector('#price');
     let addBtn = document.querySelector('#add');
+
+    let furnitureList = document.querySelector('#furniture-list');
+
+    addBtn.addEventListener('click', validateAndAdd);
+
+    function validateAndAdd(e) {
+        if (!model.value || !year.value || ! description.value || !price.value) {
+            return;
+        }
+        add(e, model.value, year.value, description.value, price.value);
+        clearInputs();
+
+    
+    }
+
+    function add(e, model, year, description, price) {
+        e.preventDefault();
+
+        let tr = htmlGenerator('tr', '', furnitureList);
+        tr.setAttribute('class', 'info');
+        htmlGenerator('td', `${model}`, tr);
+        htmlGenerator('td', `${Number(price).toFixed(2)}`, tr);
+        let td = htmlGenerator('td', '', tr);
+
+        let moreBtn = htmlGenerator('button', 'More Info', td);
+        moreBtn.setAttribute('class', 'moreBtn');
+
+        let buyBtn = htmlGenerator('button', 'Buy It', td);
+        buyBtn.setAttribute('class', 'buyBtn');
+
+    }
+
+    function htmlGenerator(tagName, content, parent) {
+        let el = document.createElement(tagName);
+        el.textContent = content;
+        if (parent) {
+            parent.appendChild(el);
+        }
+        return el;
+    }
+
+    function clearInputs() {
+        model.value = '';
+        year.value = '';
+        description.value = '';
+        price.value = '';
+    }
 }
