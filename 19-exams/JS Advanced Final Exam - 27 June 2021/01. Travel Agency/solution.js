2c1df1de0f9625f364fde3c7a809efd43cbd678a
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-11 00:27:00 +0300

    Problem Solution

--- 19-exams/JS Advanced Final Exam - 27 June 2021/01. Travel Agency/solution.js
+++ 19-exams/JS Advanced Final Exam - 27 June 2021/01. Travel Agency/solution.js
@@ -12,6 +12,7 @@ function solution() {
     const continueBtn = document.querySelector("#continueBTN");
 
     const infoPreview = document.querySelector("#infoPreview");
+    const divBlock = document.querySelector("#block");
 
 
     submitBtn.addEventListener('click', (e) => {
@@ -22,18 +23,45 @@ function solution() {
         clearInputs();
     });
 
-    function addInfo(e, fname, email, phone, address, code) {
+    function addInfo(e, fname, mail, phoneNum, addresss, code) {
         htmlGenerator('li', `Full Name: ${fname}`, infoPreview);
-        htmlGenerator('li', `Emails: ${email}`, infoPreview);
-        htmlGenerator('li', `Phone Number: ${phone}`, infoPreview);
-        htmlGenerator('li', `Address: ${address}`, infoPreview);
+        htmlGenerator('li', `Email: ${mail}`, infoPreview);
+        htmlGenerator('li', `Phone Number: ${phoneNum}`, infoPreview);
+        htmlGenerator('li', `Address: ${addresss}`, infoPreview);
         htmlGenerator('li', `Postal Code: ${code}`, infoPreview);
 
         submitBtn.setAttribute('disabled', 'disabled');
         editBtn.removeAttribute('disabled');
         continueBtn.removeAttribute('disabled');
 
+        editBtn.addEventListener('click', (e) => {
+            fullName.value = fname;
+            email.value = mail;
+            phone.value = phoneNum;
+            address.value = addresss;
+            postalCode.value = code;
+
+            submitBtn.removeAttribute('disabled');
+            editBtn.setAttribute('disabled', 'disabled');
+            continueBtn.setAttribute('disabled', 'disabled');
+
+            let allLis = infoPreview.querySelectorAll('li');
+            for (let li of allLis) {
+                li.remove();
+            }
+        })
+
+        continueBtn.addEventListener('click', (e) => { 
+            while (divBlock.firstChild) {
+                divBlock.removeChild(divBlock.lastChild);
+            }
+            htmlGenerator('h3', "Thank you for your reservation!", divBlock);
+        })
+
+
     }
+
+
 
     function htmlGenerator(tagName, content, parent) {
         let el = document.createElement(tagName);
@@ -51,4 +79,6 @@ function solution() {
         address.value = '';
         postalCode.value = '';
     }
+
+
 }
