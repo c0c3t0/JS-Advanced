c4da194688dc7c757803b10bbd95ff98c5931d8e
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-10 16:07:46 +0300

    Problem Solution

--- 19-exams/JS Advanced Final Exam - 27 June 2021/02. restaurant.js
+++ 19-exams/JS Advanced Final Exam - 27 June 2021/02. restaurant.js
@@ -7,7 +7,6 @@ class Restaurant {
     }
 
     loadProducts(products) {
-        let result = []
         for (let product of products) {
             let [productName, productQuantity, productTotalPrice] = product.split(' ');
             productQuantity = Number(productQuantity);
@@ -26,30 +25,75 @@ class Restaurant {
             } else {
                 this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
             }
-        
+
         }
         return this.history.join('\n');
 
     }
 
-    addToMenu() {
+    addToMenu(meal, neededProducts, price) {
+        if (!this.menu[meal]) {
 
+            this.menu[meal] = {
+                products: {},
+                price,
+            }
+
+            neededProducts.forEach(el => {
+                let [productName, productQuantity] = el.split(' ');
+                productQuantity = Number(productQuantity);
+                this.menu[meal].products[productName] = productQuantity;
+            });
+
+            if (Object.keys(this.menu).length === 1) {
+                return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+            } else {
+                return `Great idea! Now with the ${meal} we have ${Object.keys(this.menu).length} meals in the menu, other ideas?`;
+            }
+        } else {
+            return `The ${meal} is already in the our menu, try something different.`;
+        }
     }
 
-    showMenu() {
-
+    showTheMenu() {
+        if (Object.keys(this.menu).length === 0) {
+            return 'Our menu is not ready yet, please come later...';
+        } else {
+            let result = [];
+            for (let meal in this.menu) {
+                result.push(`${meal} - $ ${this.menu[meal].price}`);
+            }
+            return result.join('\n');
+        }
     }
 
-    makeTheOrder() {
+    makeTheOrder(meal) {
+        if (!this.menu[meal]) {
+            return `There is not ${meal} yet in our menu, do you want to order something else?`;
+        } else {
+            let products = {};
+            for (let product in this.menu[meal].products) {
+                if (!this.stockProducts[product] || this.stockProducts[product] < this.menu[meal].products[product]) {
+                    return `For the time being, we cannot complete your order (${meal}), we are very sorry...`;
+                } else {
+                    products[product] = this.menu[meal].products[product];
+                }
+            }
 
+            for (let neededProduct in products) {
+                this.stockProducts[product] -= products[product];
+            }
+
+            this.budgetMoney += this.menu[meal].price;
+            return `Your order (${meal}) will be completed in the next 30 minutes and will cost you ${this.menu[meal].price}.`;
+        }
     }
 }
 
 let kitchen = new Restaurant(1000);
-console.log(kitchen.loadProducts([
-    'Banana 10 5',
-    'Banana 20 10',
-     'Strawberries 50 30',
-      'Yogurt 10 10', 
-      'Yogurt 500 1500', 
-      'Honey 5 50']));
+console.log(kitchen.addToMenu(
+    'frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
+console.log(kitchen.addToMenu(
+    'Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1',
+    'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
+console.log(kitchen.showTheMenu());
