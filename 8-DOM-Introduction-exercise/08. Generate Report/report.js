be68f83fc1b389a92a655c7c9de8facd87fbb495
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-29 15:05:10 +0300

    updated

--- 8-DOM-Introduction-exercise/08. Generate Report/report.js
+++ 8-DOM-Introduction-exercise/08. Generate Report/report.js
@@ -2,9 +2,9 @@ function generateReport() {
     let checkboxes = document.querySelectorAll('thead tr th input');
     let rows = document.querySelectorAll('tbody tr');
     let result = [];
-    let report = {};
 
     for (let i = 0; i < rows.length; i++) {
+        let report = {};
         for (let j = 0; j < checkboxes.length; j++) {
             if (checkboxes[j].checked) {
                 report[checkboxes[j].name] = rows[i].children[j].textContent;
