df1735e264b454fe1a7c0998046f9475bf7ee366
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-09-29 15:33:02 +0300

    Changes

--- 16-classes-exercise/07. hex.js
+++ 16-classes-exercise/07. hex.js
@@ -2,23 +2,18 @@ class Hex {
     constructor(value) {
         this.value = value;
     }
-
     valueOf() {
         return this.value;
     }
-
     toString() {
-        return ("0x" + this.value.toString(16)).toUpperCase();
+        return "0x" + this.value.toString(16).toUpperCase();
     }
-
     plus(number) {
         return new Hex(this.value + number);
     }
-
     minus(number) {
         return new Hex(this.value - number);
     }
-
     parse(string) {
         return string.slice(2).toString(10);
     }
