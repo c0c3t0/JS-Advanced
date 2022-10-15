91577d60d6a424cc8d2cbda72f0bdb8002f25fd4
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-15 14:52:54 +0300

    Problem Solution

--- 19-exams/JS Advanced Retake - 08 April 2020/03. bank.js
+++ 19-exams/JS Advanced Retake - 08 April 2020/03. bank.js
@@ -73,7 +73,7 @@ class Bank {
         let result = [
             `Bank name: ${this._bankName}`,
             `Customer name: ${person.firstName} ${person.lastName}`,
-            `person ID: ${personalId}`,
+            `Customer ID: ${personalId}`,
             `Total Money: ${person.totalMoney}$`,
             `Transactions:`];
 
@@ -85,15 +85,3 @@ class Bank {
     }
 }
 
-let bank = new Bank('SoftUni Bank');
-
-console.log(bank.newCustomer({ firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267 }));
-console.log(bank.newCustomer({ firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596 }));
-
-bank.depositMoney(6233267, 250);
-console.log(bank.depositMoney(6233267, 250));
-bank.depositMoney(4151596, 555);
-
-console.log(bank.withdrawMoney(6233267, 125));
-
-console.log(bank.customerInfo(6233267));
