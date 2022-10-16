523491906353100a80d85d107d65f9a4c4f5ff27
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-17 01:30:00 +0300

    Problem Solution

--- 19-exams/JS Advanced - Exam Prep - 15.02.2021/01. Christmas Gifts Delivery/app.js
+++ 19-exams/JS Advanced - Exam Prep - 15.02.2021/01. Christmas Gifts Delivery/app.js
@@ -1,3 +1,57 @@
 function solution() {
-    //TO DO
+    document.querySelector('button').addEventListener('click', addGift);
+    const input = document.querySelector('input');
+
+    const listOfGiftsUl = document.querySelectorAll('section')[1].children[1];
+    const sentGiftsUl = document.querySelectorAll('section')[2].children[1];
+    const discardedGiftsUl = document.querySelectorAll('section')[3].children[1];
+
+    function addGift(e) {
+        e.preventDefault();
+
+        const li = htmlGenerator('li', input.value, listOfGiftsUl, 'gift');
+        const sendBtn = htmlGenerator('button', 'Send', li, '', 'sendButton');
+        sendBtn.addEventListener('click', sendGift);
+
+        const discardBtn = htmlGenerator('button', 'Discard', li, '', 'discardButton');
+        discardBtn.addEventListener('click', discardGift);
+
+
+        Array.from(listOfGiftsUl.querySelectorAll('li'))
+            .sort((a, b) => a.textContent.localeCompare(b.textContent))
+            .forEach(x => {
+                listOfGiftsUl.appendChild(x);
+            })
+        input.value = '';
+    }
+
+    function sendGift(e){
+        const currentGift = e.target.parentNode;
+        sentGiftsUl.appendChild(currentGift);
+        currentGift.querySelector('#sendButton').remove();
+        currentGift.querySelector('#discardButton').remove();
+    }
+
+    function discardGift(e) {
+        const currentGift = e.target.parentNode;
+        discardedGiftsUl.appendChild(currentGift);
+        currentGift.querySelector('#sendButton').remove();
+        currentGift.querySelector('#discardButton').remove();
+    }
+
+    function htmlGenerator(tag, text, parent, className, id) {
+        const el = document.createElement(tag);
+        el.textContent = text;
+        if (parent) {
+            parent.appendChild(el);
+        }
+        if (className) {
+            el.className = className;
+        }
+        if (id) {
+            el.setAttribute('id', id);
+        }
+
+        return el;
+    }
 }
