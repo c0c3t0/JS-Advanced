21d8dff4dd11a77774cb6021b9c55bf05ea65a64
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-17 00:37:45 +0300

    Problem Solution

--- 19-exams/JS Advanced Retake - 08 April 2020/01. taskManager/Skeleton/app.js
+++ 19-exams/JS Advanced Retake - 08 April 2020/01. taskManager/Skeleton/app.js
@@ -1,15 +1,73 @@
 function solve() {
     document.querySelector('#add').addEventListener('click', validateInputs);
+    const task = document.querySelector('#task');
+    const description = document.querySelector('#description');
+    const date = document.querySelector('#date');
 
     function validateInputs(e) {
         e.preventDefault();
-        const task = document.querySelector('#task');
-        const description = document.querySelector('#description');
-        const date = document.querySelector('#date');
 
         if (!task.value || !description.value || !date.value) {
             return;
         }
-        // TODO: After validating the input fields, you need to add the new task (article) in “Open” section.
+        addTask();
+        clearInputs();
+    }
+
+    function addTask() {
+        const openSectionDiv = document.querySelectorAll('section')[1].children[1];
+
+        const article = htmlGenerator('article', '', openSectionDiv);
+        htmlGenerator('h3', task.value, article);
+        htmlGenerator('p', `Description: ${description.value}`, article);
+        htmlGenerator('p', `Due Date: ${date.value}`, article);
+
+        const divFlex = htmlGenerator('div', '', article, 'flex');
+        const startBtn = htmlGenerator('button', 'Start', divFlex, 'green');
+        startBtn.addEventListener('click', moveToProgress);
+
+        const deleteBtn = htmlGenerator('button', 'Delete', divFlex, 'red');
+        deleteBtn.addEventListener('click', deleteTask);
+    }
+
+    function moveToProgress(e) {
+        const inProgressDiv = document.querySelector('section #in-progress');
+        const currentArticle = e.target.parentNode.parentNode;
+
+        inProgressDiv.appendChild(currentArticle);
+        currentArticle.querySelector('.green').remove();
+        const finishBtn = htmlGenerator('button', 'Finish', currentArticle.querySelector('.flex'), 'orange');
+        finishBtn.addEventListener('click', moveToComplete);
+    }
+
+    function deleteTask(e) {
+        e.target.parentNode.parentNode.remove();
+    }
+
+    function moveToComplete(e) {
+        const completeSectionDiv = document.querySelectorAll('section')[3].children[1];
+        const currentArticle = e.target.parentNode.parentNode;
+
+        completeSectionDiv.appendChild(currentArticle);
+        currentArticle.querySelector('.flex').remove();
+    }
+
+    function htmlGenerator(tag, text, parent, className) {
+        const el = document.createElement(tag);
+        el.textContent = text;
+        if (parent) {
+            parent.appendChild(el);
+        }
+        if (className) {
+            el.className = className;
+        }
+
+        return el;
+    }
+
+    function clearInputs() {
+        task.value = '';
+        description.value = '';
+        date.value = '';
     }
 }
