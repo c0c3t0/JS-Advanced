148c090a8c11dcebf910465f3f6f4dfbaf5f7729
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-17 13:52:06 +0300

    Problem Solution

--- 19-exams/JS Advanced - Exam Prep - 15.02.2021/03. dealership.test.js
+++ 19-exams/JS Advanced - Exam Prep - 15.02.2021/03. dealership.test.js
@@ -1,2 +1,22 @@
 const { expect } = require('chai');
-const dealership = require('./03. dealership')
+const dealership = require('./03. dealership');
+
+describe('dealership Tests', () => {
+    describe('newCarCost(oldCarModel, newCarPrice) tests', () => {
+        it('', () => {
+
+        });
+    });
+
+    describe('carEquipment(extrasArr, indexArr) tests', () => {
+        it('', () => {
+
+        });
+    });
+
+    describe('euroCategory(category) tests', () => {
+        it('', () => {
+
+        });
+    });
+})
